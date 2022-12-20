fb9237cf8792845e8cb6ea6ce504911388af2539
Oskar Lopez <puntoexactogeek@gmail.com>
2022-12-19 20:28:59 -0500

    Clase03 terminada

---
+++ js/validaciones.js
@@ -0,0 +1,31 @@
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+}
+
+const validadores = {
+  nacimiento: (input) => validarNacimiento(input),
+};
+
+//? setCustomValidity
+function validarNacimiento(input) {
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años de edad";
+  }
+  input.setCustomValidity(mensaje);
+}
+
+//? Método JS getUTC
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
+}
